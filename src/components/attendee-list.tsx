3d2eda78a9f3b07d98e6e4919aa8b04ddaa32715
Natanael <nata.mw@hotmail.com>
2024-04-02 15:52:36 -0300

    feat: added buttons pagination

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,4 +1,4 @@
-import { LucideSearch } from "lucide-react";
+import { LucideChevronLeft, LucideChevronRight, LucideChevronsLeft, LucideChevronsRight, LucideMoreHorizontal, LucideSearch } from "lucide-react";
 
 export function AttendeeList() {
   return (
@@ -14,12 +14,12 @@ export function AttendeeList() {
         <table className="w-full">
           <thead>
             <tr className="border border-white/10">
-              <th className="py-3 px-4 text-sm font-semibold text-left" />
+              <th style={{ width: 64}} className="py-3 px-4 text-sm font-semibold text-left" />
               <th className="py-3 px-4 text-sm font-semibold text-left">Código</th>
               <th className="py-3 px-4 text-sm font-semibold text-left">Participante</th>
               <th className="py-3 px-4 text-sm font-semibold text-left">Data de inscrição</th>
               <th className="py-3 px-4 text-sm font-semibold text-left">Data do check-in</th>
-              <th className="py-3 px-4 text-sm font-semibold text-left"></th>
+              <th style={{ width: 64}} className="py-3 px-4 text-sm font-semibold text-left"></th>
             </tr>
           </thead>
           <tbody>
@@ -37,7 +37,11 @@ export function AttendeeList() {
                 </td>
                 <td className="py-3 px-4 text-sm text-zinc-300">7 dias</td>
                 <td className="py-3 px-4 text-sm text-zinc-300">3 dias atras</td>
-                <td className="py-3 px-4 text-sm text-zinc-300"></td>
+                <td className="py-3 px-4 text-sm text-zinc-300">
+                  <button className="bg-black border border-white/10 rounded-md p-1.5">
+                    <LucideMoreHorizontal className="size-4" />
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
@@ -47,7 +51,24 @@ export function AttendeeList() {
                 Mostrando 10 de 100
               </td>
               <td className="py-3 px-4 text-sm text-zinc-300 text-right" colSpan={3}>
-                Pagina 1 de 10
+                <div className="inline-flex items-center gap-8">
+                  <span>Pagina 1 de 10</span>
+
+                  <div className="flex gap-1.5">
+                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                      <LucideChevronsLeft className="size-4" />
+                    </button>
+                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                      <LucideChevronLeft className="size-4" />
+                    </button>
+                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                      <LucideChevronRight className="size-4" />
+                    </button>
+                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                      <LucideChevronsRight className="size-4" />
+                    </button>
+                  </div>
+                </div>
               </td>
             </tr>
           </tfoot>
