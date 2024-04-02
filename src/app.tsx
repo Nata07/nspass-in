d70d8799aac6d00684beb0a4cebd3bab98c5cb88
Natanael <nata.mw@hotmail.com>
2024-04-02 12:32:54 -0300

    chore: init config project

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import { AttendeeList } from "./components/attendee-list";
+import { Header } from "./components/header";
+
+export function App() {
+  return (
+    <div className="max-w-7xl mx-auto py-5 px-4">
+      <Header />
+      <AttendeeList />
+    </div>
+  )
+}
---
+++ src/components/attendee-list.tsx
@@ -0,0 +1,7 @@
+export function AttendeeList() {
+  return (
+    <div>
+      <h1>Participante</h1>
+    </div>
+  )
+}
