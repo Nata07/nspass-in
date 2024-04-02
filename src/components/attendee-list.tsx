52076acb0dcc155520a21857503b4d0bed37f732
Natanael <nata.mw@hotmail.com>
2024-04-02 12:51:22 -0300

    feat: added header pages

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,7 +1,13 @@
+import { LucideSearch } from "lucide-react";
+
 export function AttendeeList() {
   return (
-    <div>
-      <h1>Participante</h1>
+    <div className="flex items-center gap-3">
+      <h1 className="text-2xl font-bold">Participantes</h1>
+      <div className="flex items-center gap-3 px-3 bg-transparent w-72 py-1.5 border border-white/10 rounded-lg text-sm">
+        <LucideSearch className="size-4 text-emerald-300" />
+        <input className="bg-transparent outline-none" type="text" placeholder="Buscar participantes" />
+      </div>
     </div>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,13 @@
+import Logo from "../assets/logo-icon.svg"
+
 export function Header() {
   return (
-    <header>
-      header
+    <header className="flex gap-5 items-center py-2">
+      <img src={Logo} alt="Logo NSPass-IN" />
+      <nav className="flex items-center gap-5">
+        <a href="" className="font-medium text-sm text-zinc-400">Eventos</a>
+        <a href="" className="font-medium text-sm">Participantes</a>
+      </nav>
     </header>
   )
 }
