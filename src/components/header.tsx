2bb13580c83a658ef6f8fcaa9628fd38b577e732
Natanael <nata.mw@hotmail.com>
2024-04-02 22:32:58 -0300

    feat: added component nav link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,13 @@
 import Logo from "../assets/logo-icon.svg"
+import { NavLink } from "./nav-link"
 
 export function Header() {
   return (
     <header className="flex gap-5 items-center py-2">
       <img src={Logo} alt="Logo NSPass-IN" />
       <nav className="flex items-center gap-5">
-        <a href="" className="font-medium text-sm text-zinc-400">Eventos</a>
-        <a href="" className="font-medium text-sm">Participantes</a>
+        <NavLink href="/eventos">Eventos</NavLink>
+        <NavLink href="/participantes">Participantes</NavLink>
       </nav>
     </header>
   )
